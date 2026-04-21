349e6fb21ae6c0f10eb1567039b6ef6979cc2fab
usergdp136 <129577191+usergdp136@users.noreply.github.com>
2026-04-21 22:17:56 +0200

    Add preview configuration to vite.config.js
    Added preview configuration for hosting and port settings.

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: 8080,
+    allowedHosts: ['monopolygdp-production.up.railway.app']
+  }
 })
